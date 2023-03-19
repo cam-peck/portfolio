e68453ad71c839317a13178590307ed841605ee9
Cameron Peck <cameronpeck317@gmail.com>
2023-03-19 19:58:42 +0000

    add boilerplate for pages and add navbar component and tests

---
+++ pages/__tests__/navbar.test.tsx
@@ -0,0 +1,45 @@
+import { render, screen } from '@testing-library/react';
+import NavBar from '../components/navbar';
+import '@testing-library/jest-dom';
+
+describe('NavBar', () => {
+  it('renders the CP emblem in the top right corner', () => {
+    render(<NavBar />);
+
+    const cpEmblem = screen.getByAltText('navbar-emblem');
+
+    expect(cpEmblem).toBeInTheDocument();
+  });
+
+  it('contains four links to Home, About, Projects, and Contact', () => {
+    render(<NavBar />);
+
+    const homeLink = screen.getByText('Home');
+    const aboutLink = screen.getByText('About');
+    const projectsLink = screen.getByText('Projects');
+    const contactLink = screen.getByText('Contact');
+
+    expect(homeLink).toHaveAttribute('href', '/');
+    expect(aboutLink).toHaveAttribute('href', '/about');
+    expect(projectsLink).toHaveAttribute('href', '/projects');
+    expect(contactLink).toHaveAttribute('href', '/contact');
+  });
+
+  it('contains a hidden hamburger menu', () => {
+    render(<NavBar />);
+
+    const hamburgerMenu = screen.getByTestId('navbar-hamburger-menu');
+
+    expect(hamburgerMenu).not.toBeVisible();
+  });
+
+  it('contains a hidden hamburger menu', () => {
+    render(<NavBar />);
+
+    const hamburgerMenu = screen.getByTestId('navbar-hamburger-menu', {
+      hidden: true,
+    });
+
+    expect(hamburgerMenu).not.toBeVisible();
+  });
+});
