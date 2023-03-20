12b5ee0cea9eb7c897637866a8decaa2603e5b12
Cameron Peck <cameronpeck317@gmail.com>
2023-03-20 03:10:29 +0000

    add custom color pallete to tailwindcss config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,18 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      ghostwhite: '#F4F4F9',
+      lightblue: '#B8DBD9',
+      paynesgray: '586F7C',
+      charcoal: '#2F4550',
+      black: '#000000',
+      'success-green': '#28A745',
+      'danger-red': '#DC3545',
+      orange: '#FFB017',
+      coral: '#FF8691',
+      lightpurple: '#A259FE',
+    },
   },
   plugins: [],
 };
