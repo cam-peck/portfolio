3e1f0762707c6b91c7803d73b780a816adefd718
Cameron Peck <cameronpeck317@gmail.com>
2023-03-20 14:31:03 +0000

    add hamburgermenu component with animations

---
+++ components/hamburger-menu.tsx
@@ -0,0 +1,29 @@
+interface HamburgerMenuProps {
+  hamburgerIsOpen: boolean;
+  setHamburgerIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function HamburgerMenu(props: HamburgerMenuProps) {
+  const { hamburgerIsOpen, setHamburgerIsOpen } = props;
+
+  const topHamburger = 'block h-0.5 w-8 bg-ghostwhite transition-all duration-500 ease-in-out';
+  const topCloseIcon = 'block h-0.5 w-8 bg-ghostwhite rotate-45 translate-y-3 transition-all duration-500 ease-in-out';
+
+  const middleHamburger = 'block h-0.5 w-8 bg-ghostwhite transition-all duration-500 ease-in-out';
+  const middleCloseIcon = 'block h-0.5 w-8 bg-black -translate-x-8 transition-all duration-500 ease-in-out';
+
+  const bottomHamburger = 'block h-0.5 w-8 bg-ghostwhite transition-all duration-500 ease-in-out';
+  const bottomCloseIcon = 'block h-0.5 w-8 bg-ghostwhite -rotate-45 -translate-y-2 transition-all duration-500 ease-in-out';
+  return (
+    <section>
+      <div onClick={() => setHamburgerIsOpen(!hamburgerIsOpen)} className={`space-y-2 py-4 hover:cursor-pointer`}>
+        {/* Top Bar*/}
+        <span className={hamburgerIsOpen ? topHamburger : topCloseIcon}></span>
+        {/* Middle Bar */}
+        <span className={hamburgerIsOpen ? middleHamburger : middleCloseIcon}></span>
+        {/* Bottom Bar*/}
+        <span className={hamburgerIsOpen ? bottomHamburger : bottomCloseIcon}></span>
+      </div>
+    </section>
+  );
+}
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,15 @@
+import { useState } from 'react';
 import NavItems from './navitems';
+import HamburgerMenu from './hamburger-menu';
 
 export default function NavBar() {
+  const [hamburgerIsOpen, setHamburgerIsOpen] = useState(false);
   return (
     <header className="w-full bg-black text-ghostwhite">
       <nav className="max-w-6xl px-6 mx-auto flex items-center justify-between">
         {/* Logo */}
         <div className="flex gap-3 items-center">
-          {/* <img /> logo will go here!*/}
+          {/* <img /> logo will go here instead of h1!*/}
           <h1 className="text-white text-xl x2s:text-2xl">CP</h1>
         </div>
         {/* Menu Items */}
@@ -14,14 +17,10 @@ export default function NavBar() {
           <NavItems />
         </div>
         {/* Hamburger Menu */}
-        {/* <div className="md:hidden pt-2 pb-2">
-          <Hamburger
-            size={28}
-            toggled={navDrawerIsOpen}
-            toggle={setNavDrawerIsOpen}
-            color="white"
-          />
+        <div className="md:hidden pt-2 pb-2">
+          <HamburgerMenu hamburgerIsOpen={hamburgerIsOpen} setHamburgerIsOpen={setHamburgerIsOpen} />
         </div>
+        {/* 
         <DrawerMenu
           setNavDrawerIsOpen={setNavDrawerIsOpen}
           navDrawerIsOpen={navDrawerIsOpen}
