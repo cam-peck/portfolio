ca5fcdad07143d4cad5bb335ab8e89104e2d5228
Cameron Peck <cameronpeck317@gmail.com>
2023-04-03 18:24:57 +0000

    add header and ensure projects page renders correctly

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,7 +1,11 @@
 export default function Projects() {
   return (
-    <div>
-      <h1>This is the projects page</h1>
-    </div>
+    <main className="max-w-6xl mx-auto px-6 pt-4 mdlg:pt-8">
+      <div className="mb-16 pb-2 border-b-2">
+        <h1 data-testid="main-header" className="text-[48px] font-bold text-charcoal text-center mdlg:text-left">
+          Projects
+        </h1>
+      </div>
+    </main>
   );
 }
