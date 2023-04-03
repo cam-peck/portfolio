9aa3102eb1cf7cb9d97a9d7985b18e34490d4355
Cameron Peck <cameronpeck317@gmail.com>
2023-04-03 15:58:53 +0000

    add title and main image for about me page with scroll for overflow on mobile and smaller screens.

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,16 @@
+import Image from 'next/image';
+
 export default function About() {
   return (
-    <div>
-      <h1>This is the about page</h1>
-    </div>
+    <main className="max-w-6xl mx-auto px-6 pt-4 mdlg:pt-8">
+      <div className="mb-16 pb-2 border-b-2">
+        <h1 data-testid="main-header" className="text-[48px] font-bold text-charcoal text-center mdlg:text-left">
+          About Me
+        </h1>
+      </div>
+      <div className="w-full mx-auto overflow-x-scroll">
+        <Image className="min-w-[1100px]" src="/about-me.png" width={1600} height={1600} alt="profile-image" />
+      </div>
+    </main>
   );
 }
